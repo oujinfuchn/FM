ce745b690bb1d3ccc4bee5cda0a0b2d66def033b
oujinfu <oujinfuchn@163.com>
2017-03-20 10:06:45 +0800

    初始化

---
+++ FM-day01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FM-day01
+//
+//  Created by Edison on 17/3/20.
+//  Copyright © 2017年 Edison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FM-day01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FM-day01
+//
+//  Created by Edison on 17/3/20.
+//  Copyright © 2017年 Edison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
